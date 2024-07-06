cf9434bce9ea53ad7e9d5e45b4627f23e3b479a8
sasha <sasha.2000ibr@gmail.com>
2024-07-06 18:39:07 +0300

    атата

--- engines/cura.py
+++ engines/cura.py
@@ -7,9 +7,10 @@ from enums import Result
 
 class CuraEngineSlicer:
 
-    def __init__(self, engine_path: str, file_path):
+    def __init__(self, engine_path: str, file_path, **slice_params):
         self._engine_path = engine_path
         self._file_path = file_path
+        self.slice_params = slice_params or None
 
     @property
     def file_path(self):
@@ -23,7 +24,8 @@ class CuraEngineSlicer:
 
     def slice(self, printer: str, **parameters) -> Result:
         assert self.file_path is not None, FileNotFoundError('File not loaded')
-
+        if not parameters:
+            parameters = self.slice_params
         try:
             result_sub = subprocess.run(self.command(printer, **parameters), cwd=self._engine_path,
                                         capture_output=True, text=True, check=True)
