def27687478848fb6c86b2d4d59294afa3b769e9
Skelesheit <Skelesheit@gmail.com>
2024-07-06 18:49:03 +0300

    ошибки при тесте

--- test.py
+++ test.py
@@ -3,9 +3,18 @@ from dataclasses import asdict
 from engines.cura import CuraEngineSlicer
 from enums import Printers, SliceParameters
 
+parameters = params = SliceParameters(
+    pattern='LINEAR',
+    speed_print=60,
+    roofing_layer_count=3,
+    count_wall_layer=2,
+    layer_height=0.2,
+    wall_thickness=1.2,
+    infill_density=20.0
+)
 print(asdict(SliceParameters()))
 
-engines = CuraEngineSlicer('CURA PATH', file_path='PATH')
+engines = CuraEngineSlicer(r'D:\UltiMaker Cura 5.7.2\CuraEngine.exe', file_path=r'D:\STL\Cura 5.7.2.stl')
 # engines.file_path = PATH
 result = engines.slice(Printers.CUSTOM.value, **asdict(SliceParameters()))
 print(result)
